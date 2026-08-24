c60f8d357dab576a70fd6a58fd8583c8abb572d7
Divyam Gawde <140106819+divyam-gawde@users.noreply.github.com>
2026-08-24 16:01:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,26 @@
 pkg update -y
 pkg upgrade -y
 pkg install -y proot-distro
+
 proot-distro install ubuntu:24.04
 
 proot-distro login ubuntu -- bash -lc '
 apt update &&
 apt upgrade -y &&
-apt install -y python3 python3-pip python3-venv python3-dev build-essential tmux &&
+apt install -y python3 python3-pip python3-venv python3-dev build-essential &&
 python3 -m venv ~/lncrawl-env &&
 source ~/lncrawl-env/bin/activate &&
 python -m pip install -U pip setuptools wheel &&
-python -m pip install -U lightnovel-crawler &&
-tmux new-session -d -s lncrawl "source ~/lncrawl-env/bin/activate && exec lncrawl -ll server --host 0.0.0.0 --port 8181"
+python -m pip install -U lightnovel-crawler
 '
 
 termux-wake-lock
 
 echo
 echo "======================================"
-echo " lncrawl is starting"
-echo " http://127.0.0.1:8181"
+echo " Installation complete!"
+echo " Starting lncrawl..."
 echo "======================================"
+echo
+
+proot-distro login ubuntu -- bash -lc 'source ~/lncrawl-env/bin/activate && lncrawl -ll server --host 0.0.0.0 --port 8181'
